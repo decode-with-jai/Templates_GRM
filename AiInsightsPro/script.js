0f00c93a107a66f90672f084abcb3da3818f3907
jaiguar <jaiguaryt@gmail.com>
2026-04-11 11:09:06 +0530

    upload last 2 months projects

---
+++ AiInsightsPro/script.js
@@ -0,0 +1,5 @@
+const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+learnMoreBtn.addEventListener("click", function () {
+    alert("Welcome to AIInsightsPro! Explore the platform and unlock powerful AI insights.");
+});
